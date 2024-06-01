f55b4902002720ef9114109c45cd7980db7eec16
felipeog <17603069+felipeog@users.noreply.github.com>
2024-06-01 01:41:16 -0300

    save and load preset

--- js/_constants.js
+++ js/_constants.js
@@ -7,3 +7,5 @@ export const MAX_GENERATIONS = 10;
 export const ONE_SECOND_IN_MS = 1_000;
 
 export const GENERATIONS_PER_SECOND = 10;
+
+export const LOCAL_STORAGE_KEY = "gof-preset";
--- js/_gui.js
+++ js/_gui.js
@@ -5,81 +5,97 @@ import {
   createRandomGeneration,
 } from "./_functions.js";
 import { state } from "./_state.js";
+import { LOCAL_STORAGE_KEY } from "./_constants.js";
 
 export function createGui() {
   const gui = new GUI();
+  const properties = {
+    random() {
+      const randomGeneration = createRandomGeneration();
+
+      state.generations = [randomGeneration];
+    },
+    gosperGliderGun() {
+      const generation = createEmptyGeneration();
+
+      generation[0][24] = 1;
+      generation[1][22] = 1;
+      generation[1][24] = 1;
+      generation[2][12] = 1;
+      generation[2][13] = 1;
+      generation[2][20] = 1;
+      generation[2][21] = 1;
+      generation[2][34] = 1;
+      generation[2][35] = 1;
+      generation[3][11] = 1;
+      generation[3][15] = 1;
+      generation[3][20] = 1;
+      generation[3][21] = 1;
+      generation[3][34] = 1;
+      generation[3][35] = 1;
+      generation[4][0] = 1;
+      generation[4][1] = 1;
+      generation[4][10] = 1;
+      generation[4][16] = 1;
+      generation[4][20] = 1;
+      generation[4][21] = 1;
+      generation[5][0] = 1;
+      generation[5][1] = 1;
+      generation[5][10] = 1;
+      generation[5][14] = 1;
+      generation[5][16] = 1;
+      generation[5][17] = 1;
+      generation[5][22] = 1;
+      generation[5][24] = 1;
+      generation[6][10] = 1;
+      generation[6][16] = 1;
+      generation[6][24] = 1;
+      generation[7][11] = 1;
+      generation[7][15] = 1;
+      generation[8][12] = 1;
+      generation[8][13] = 1;
+
+      state.generations = [generation];
+    },
+    generationsPerSecond: state.generationsPerSecond,
+    savePreset() {
+      const preset = gui.save();
+
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(preset));
+      loadPresetButton.enable();
+    },
+    loadPreset() {
+      if (!localStorage.getItem(LOCAL_STORAGE_KEY)) return;
+
+      try {
+        const preset = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+        gui.load(preset);
+      } catch (error) {
+        loadPresetButton.disable();
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        console.error(error);
+      }
+    },
+  };
+
+  gui.add(properties, "random").name("Random");
+
+  gui.add(properties, "gosperGliderGun").name("Gosper glider gun");
 
   gui
-    .add(
-      {
-        callback() {
-          const randomGeneration = createRandomGeneration();
-          state.generations = [randomGeneration];
-        },
-      },
-      "callback"
-    )
-    .name("Random");
-
-  gui
-    .add(
-      {
-        callback() {
-          const gosperGliderGun = createEmptyGeneration();
-
-          gosperGliderGun[0][24] = 1;
-          gosperGliderGun[1][22] = 1;
-          gosperGliderGun[1][24] = 1;
-          gosperGliderGun[2][12] = 1;
-          gosperGliderGun[2][13] = 1;
-          gosperGliderGun[2][20] = 1;
-          gosperGliderGun[2][21] = 1;
-          gosperGliderGun[2][34] = 1;
-          gosperGliderGun[2][35] = 1;
-          gosperGliderGun[3][11] = 1;
-          gosperGliderGun[3][15] = 1;
-          gosperGliderGun[3][20] = 1;
-          gosperGliderGun[3][21] = 1;
-          gosperGliderGun[3][34] = 1;
-          gosperGliderGun[3][35] = 1;
-          gosperGliderGun[4][0] = 1;
-          gosperGliderGun[4][1] = 1;
-          gosperGliderGun[4][10] = 1;
-          gosperGliderGun[4][16] = 1;
-          gosperGliderGun[4][20] = 1;
-          gosperGliderGun[4][21] = 1;
-          gosperGliderGun[5][0] = 1;
-          gosperGliderGun[5][1] = 1;
-          gosperGliderGun[5][10] = 1;
-          gosperGliderGun[5][14] = 1;
-          gosperGliderGun[5][16] = 1;
-          gosperGliderGun[5][17] = 1;
-          gosperGliderGun[5][22] = 1;
-          gosperGliderGun[5][24] = 1;
-          gosperGliderGun[6][10] = 1;
-          gosperGliderGun[6][16] = 1;
-          gosperGliderGun[6][24] = 1;
-          gosperGliderGun[7][11] = 1;
-          gosperGliderGun[7][15] = 1;
-          gosperGliderGun[8][12] = 1;
-          gosperGliderGun[8][13] = 1;
-
-          state.generations = [gosperGliderGun];
-        },
-      },
-      "callback"
-    )
-    .name("Gosper glider gun");
-
-  gui
-    .add({ value: state.generationsPerSecond }, "value")
-    .step(1)
-    .min(1)
-    .max(30)
+    .add(properties, "generationsPerSecond", 1, 30, 1)
     .name("Generations per second")
     .onFinishChange((value) => {
       if (state.animateTimeoutId) clearTimeout(state.animateTimeoutId);
       state.generationsPerSecond = value;
       animate();
     });
+
+  gui.add(properties, "savePreset").name("Save preset");
+
+  const loadPresetButton = gui
+    .add(properties, "loadPreset")
+    .name("Load preset")
+    .disable(!localStorage.getItem(LOCAL_STORAGE_KEY));
 }
